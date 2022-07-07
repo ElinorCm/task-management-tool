c743b5cbf36ace329a2e72a677e3484e2e364317
ElinorCm <elinor.compagnon@gmail.com>
2022-07-07 10:04:17 +0200

    feat: static forms

--- front-react/src/components/Lists/List/index.js
+++ front-react/src/components/Lists/List/index.js
@@ -1,12 +1,16 @@
 import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import Container from '@mui/material/Container';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditIcon from '@mui/icons-material/Edit';
 import Paper from '@mui/material/Paper';
-import CardActions from '@mui/material/CardActions';
+import TextField from '@mui/material/TextField';
+
+import './list.scss';
 
 function List() {
   return (
@@ -26,6 +30,12 @@ function List() {
           <DeleteOutlineIcon />
         </CardActions>
       </Container>
+      <Container>
+        <form className="list-form">
+          <TextField id="name" label="Nom de la liste" variant="standard" />
+          <Button variant="contained">Valider</Button>
+        </form>
+      </Container>
       <CardContent>
         <Card sx={{ background: 'white' }}>
           <Container sx={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -37,6 +47,12 @@ function List() {
               <EditIcon />
               <DeleteOutlineIcon />
             </CardActions>
+          </Container>
+          <Container sx={{ display: 'flex' }}>
+            <form className="card-form">
+              <TextField id="name" label="Nom de la carte" variant="standard" />
+              <Button variant="contained">Valider</Button>
+            </form>
           </Container>
         </Card>
       </CardContent>
